cd6c8db0a449201c825c45948b891928bbce2df0
13901 <1390109557@qq.com>
2020-04-29 22:12:54 +0800

    add test

--- test.py
+++ test.py
@@ -1,8 +1,6 @@
-import requests
-def downloadDanMu():
-    url = "http://comment.bilibili.com/{}.xml".format(15954941)
-    html = requests.get(url)
-    with open("111.xml", "wb") as f:
-        f.write(html.content)
-    print(html.text)
-downloadDanMu()
+import bilibili
+
+bili = bilibili.BiliBli()
+bili.set_url("https://www.bilibili.com/video/BV1Up4y1X7D1")
+bili.set_Thread(32)
+bili.Go()
